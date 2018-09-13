ca4731d4055b3b230eb94361979f30ffdc5a0aaf
Nic <nic@nicsknickknackshack.com>
2018-09-13 18:41:29 -0400

    Add one space after ending curly bracket of functions

--- Source/ProjectDugong/TileMap.cpp
+++ Source/ProjectDugong/TileMap.cpp
@@ -34,6 +34,7 @@ void ATileMap::GenerateMap() {
 		}
 	}
 }
+
 void ATileMap::SpawnMap() {
 	for (ATile* tile : map) {
 		//GetWorld()->SpawnActor<ATile>(tile->GetActorLocation, tile->GetActorRotation);
