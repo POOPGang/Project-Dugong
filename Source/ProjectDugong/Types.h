fe89dc4b00852ece7b7740e07a7cad5222b7a05d
MrLever <MrLeverx@gmail.com>
2018-09-28 14:10:08 -0400

    Give point type a copy contrsuctor

--- Source/ProjectDugong/Types.h
+++ Source/ProjectDugong/Types.h
@@ -4,7 +4,13 @@
 typedef struct Point {
 	int x;
 	int y;
+	//Defualt constructor
 	Point() {}
+
+	//Standard constructor
 	Point(int i, int j) : x(i), y(j) {}
+
+	//Copy constructor
+	Point(const Point &p) { x = p.x; y = p.y; }
 
 } Point;
