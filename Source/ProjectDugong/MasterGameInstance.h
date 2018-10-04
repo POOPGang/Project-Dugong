d9662361160e6498b41982531b08acfc86e9514a
SmilesBlue <jontowne11@gmail.com>
2018-10-03 20:34:12 -0400

    Prototype overworld

--- Source/ProjectDugong/MasterGameInstance.h
+++ Source/ProjectDugong/MasterGameInstance.h
@@ -6,6 +6,7 @@ class ATileMap;
 class ABaseUnit;
 class ABaseTile;
 
+#include "GameFramework/Actor.h"
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "MasterGameInstance.generated.h"
@@ -17,6 +18,15 @@ UCLASS()
 class PROJECTDUGONG_API UMasterGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditAnywhere)
+	int mysterium = 0;
+	UPROPERTY(EditAnywhere)
+	FVector newPlayerLocation;
+	UPROPERTY(EditAnywhere)
+	float DOOOM = 50.0;
+	UPROPERTY(EditAnywhere)
+	AActor newNodeSelected;
 	
 };
