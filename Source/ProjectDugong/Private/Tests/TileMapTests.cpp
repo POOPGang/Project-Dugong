b286c69ad198a7a052d4d41d27fe65f1367a9e64
Nic <nic@nicsknickknackshack.com>
2018-10-07 15:36:03 -0400

    Add includes to fix dependency issues

--- Source/ProjectDugong/Private/Tests/TileMapTests.cpp
+++ Source/ProjectDugong/Private/Tests/TileMapTests.cpp
@@ -3,20 +3,32 @@
 #include "TileMapTests.h"
 
 #include "TileMap.h"
+
+#include "Engine/World.h"
+
 #include "AutomationTest.h"
+#include "AutomationEditorCommon.h"
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTileMapTest, "Test.TileMapUnitTests", EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
 
-bool FTileMapTest::RunTest(const FString& Parameters)
-{
-	//UE_LOG(LogTemp, Warning, TEXT("Before Hot-Reload"));
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTileMapConstructorTest, "Test.TileMap.UnitTests.OperatorOverloads", EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter)
+
+bool FTileMapConstructorTest::RunTest(const FString& Parameters){
+	UE_LOG(LogTemp, Log, TEXT("Begin TileMap ctor test"));
+	
+	FVector location(0, 0, 0);
+	FRotator rotation(0, 0, 0);
+	FActorSpawnParameters spawnInfo;
+
+	//Create a temporary world
+	//UWorld* world = FAutomationEditorCommonUtils::CreateNewMap();
+
+	//Construct a TileMap
+	//ATileMap* tileMap = world->SpawnActor<ATileMap>(location, rotation, spawnInfo);
 
 	return true;
 }
 
 
+
 //Dummy ctor
-ATileMapTests::ATileMapTests() {
-
-
-}
+ATileMapTests::ATileMapTests() {}
